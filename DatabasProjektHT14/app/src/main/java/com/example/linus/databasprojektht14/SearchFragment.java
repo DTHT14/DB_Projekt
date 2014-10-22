fb4cfba5dd9306672adc9b6354503ba4f095608a
trulsson90 <trulsson90@hotmail.com>
2014-10-22 15:32:34 +0200

    ResultFragment
    Almost done

--- DatabasProjektHT14/app/src/main/java/com/example/linus/databasprojektht14/SearchFragment.java
+++ DatabasProjektHT14/app/src/main/java/com/example/linus/databasprojektht14/SearchFragment.java
@@ -1,9 +1,7 @@
 package com.example.linus.databasprojektht14;
 
-
-
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +9,7 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 
 import java.util.Calendar;
 
